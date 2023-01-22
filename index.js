a49d477d999fd49b0528a2288035b2536bb04355
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-01-22 20:13:17 +0000

    adds code

--- index.js
+++ index.js
@@ -5,17 +5,89 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'fullName',
+        message: 'What is yout full name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: function(email)
+        {
+            // Check if the user input is a valid email
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubName',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'repoName',
+        message: 'What is the name of your Github repository?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description for your project.'
+    },
+    {
+        type: "checkbox",
+        name: "technologies",
+        message: "What are the technologies you have used in this project?",
+        choices: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'jQuery', 'Node.js', 'React', 'Express.js']
+    },
+    {
+        type: 'input',
+        name: 'installDependencies',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using for this project?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data),
+        (err) => err ? console.error(err) : console.log("\nYour ReadMe has been created."))
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((res) => writeToFile("README.md", res));
 }
 
 // function call to initialize program
 init();
+
+// {
+//     type: '',
+//     name: '',
+//     message:
+// },
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,74 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![License](https://img.shields.io/badge/license-${data.license}-blue)
+  
+  ## Description
 
+  ${data.description}
+
+  ### Table of contents
+
+  * [Technologies](#technologies)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Technologies
+
+  This project is created with:
+  
+  ${data.technologies}
+
+  ## Installation
+  
+  To install, run the following commands:
+
+  1. Clone this repo to your local environment:
+  \`\`\`
+  git clone https://github.com/${data.githubName}/${data.repoName}.git
+  \`\`\`
+
+  2. Install NPM packages
+  \`\`\`
+  ${data.installDependencies}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} License.
+
+  ## Contributing
+
+  Do you have any suggestions? To contribute to this repo, follow the nest few steps:
+  1. Fork the project (https://github.com/${data.githubName}/${data.repoName})
+  2. Create a new branch - \`\git checout -b feature/branch_name\`\
+  3. Commit your code on a separate branch - \`\git commit -m 'adds new feature'\`\
+  4. Push to the branch - \`\git push origin feature/branch_name\`\
+  5. Open a new Pull Request
+  
+  I will do my best to review your suggestion(s) in due time and merge it if all is well.
+
+  ## Tests
+
+  To run tests, run the following command:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about this repository, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.githubName}](https://github.com/${data.githubName}).
+
+  ---
+  Copyright © 2023, ${data.fullName}
 `;
 }
 
